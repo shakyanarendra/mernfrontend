71a8ce76f9a8ab26af4f2d3d7ff74226a6d0aba2
shakyanarendra <133335362+shakyanarendra@users.noreply.github.com>
2025-02-22 16:26:10 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+
+
+
+const Home = () => {
+  return (
+    <div className="h-heightWithoutNavbar">
+      <Hero />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
